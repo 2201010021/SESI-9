e86b55ac6bd561b09cb16385a382ec81db19d95a
2201010021 <127836634+2201010021@users.noreply.github.com>
2023-05-09 17:04:59 +0800

    Add files via upload

---
+++ Javascript02.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2</title>
+</head>
+<body>
+    
+    <div id="teks1">
+        <p> Mata Kuliah Pada Hari Selasa Adalah Pemrograman Web </p>
+    </div>
+    <div id="teks2">
+        <p> Mata Kuliah Pemrograman Web Ini di Mulai Pada Pukul 14.50 </p>
+    </div>
+
+    <script src="Javascript02.js"></script>    
+</body>
+</html>
